3852c1971e08843d90391c376e24c44b04477b72
amelia-b <amelia_b@mit.edu>
2024-03-18 10:54:30 -0400

    projects page and pie chart for lab 6

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -1,8 +1,9 @@
 <script>
     import * as d3 from "d3";
+
     let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-    let data = [1, 2, 3, 4, 5, 5];
+    export let data = [];
 
     let total = 0;
 
@@ -17,23 +18,97 @@
         arcData.push({ startAngle: angle, endAngle });
         angle = endAngle;
     }
-    let arcs = arcData.map((d) => arcGenerator(d));
+
+    let sliceGenerator = d3.pie().value((d) => d.value);
+
+    let arcs;
+    $: {
+        arcData = sliceGenerator(data);
+        arcs = arcData.map((d) => arcGenerator(d));
+    }
     let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
+    export let selectedIndex = -1;
+
+    function toggleWedge(index, event) {
+        if (!event.key || event.key === "Enter") {
+            selectedIndex = index;
+        }
+    }
 </script>
 
-<svg viewBox="-50 -50 100 100">
-    {#each arcs as arc, i}
-        <path d={arc} fill={colors(i)} />
-    {/each}
-</svg>
+<div class="container">
+    <svg viewBox="-50 -50 100 100">
+        {#each arcs as arc, index}
+            <path
+                d={arc}
+                fill={colors(index)}
+                class:selected={selectedIndex === index}
+                on:click={(e) => toggleWedge(index, e)}
+                on:keyup={(e) => toggleWedge(index, e)}
+                tabindex="0"
+                role="button"
+                aria-label
+            />
+        {/each}
+    </svg>
+    <ul class="legend">
+        {#each data as d, index}
+            <li style="--color: {colors(index)}">
+                <span class="swatch"></span>
+                {d.label} <em>({d.value})</em>
+            </li>
+        {/each}
+    </ul>
+</div>
 
 <style>
     svg {
         max-width: 20em;
         margin-block: 2em;
+    }
 
-        /* Do not clip shapes outside the viewBox */
-        overflow: visible;
+    svg:has(path:hover, path:focus-visible) {
+        path:not(:hover, :focus-visible) {
+            opacity: 50%;
+        }
+    }
+
+    path {
+        transition: 300ms;
+        cursor: pointer;
+        outline: none;
+    }
+
+    .selected {
+        --color: oklch(60% 40% 200) !important;
+
+        &:is(path) {
+            fill: var(--color);
+        }
+    }
+
+    .container {
+        display: flex;
+        align-items: center; /* Center items vertically */
+    }
+
+    .legend {
+        /* flex: 1; */
+        list-style: none;
+        border: 1px solid black;
+        padding: 10px;
+        border-radius: 5px;
+        /* align-items: 'flex-start'; */
+        margin-left: 20px;
+    }
+
+    .legend li .swatch {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        border: 1px solid black;
+        margin-right: 5px;
+        background-color: var(--color);
     }
 </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,21 +1,78 @@
-
-
 <script>
+  import * as d3 from "d3";
+
   /** @type {import('./$types').PageData} */
   export let data;
   import projects from "$lib/projects.json";
   import Project from "$lib/Project.svelte";
-  import Pie from '$lib/Pie.svelte';
+  import Pie from "$lib/Pie.svelte";
+  let query = "";
+
+  let filteredProjects;
+
+  // Make sure the variable definition is *outside* the block
+  let pieData;
+  let projectsToShow;
+  let selectedYearIndex = -1;
+  let selectedYear;
+
+  $: {
+    // Initialize to an empty object every time this runs
+    pieData = {};
+
+    // Calculate rolledData and pieData based on filteredProjects here
+    let rolledData = d3.rollups(
+      filteredProjects,
+      (v) => v.length,
+      (d) => d.year,
+    );
+    pieData = rolledData.map(([year, count]) => {
+      return { value: count, label: year };
+    });
+  }
+
+  $: filteredProjects = projects.filter((project) => {
+    let values = Object.values(project).join("\n").toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+
+  $: {
+    let filteredByYear = projects.filter((project) => {
+      return project.year === selectedYear;
+    });
+
+    selectedYear =
+      selectedYearIndex > -1 ? pieData[selectedYearIndex].label : null;
+
+    projectsToShow =
+      selectedYear != undefined ? filteredByYear : filteredProjects;
+
+    console.log("Year", selectedYear, "type:", typeof selectedYear);
+
+    console.log("selectedYearIndex", selectedYearIndex);
+    console.log("projects", projects)
+    console.log("projectsToShow", projectsToShow);
+  }
 </script>
 
 <svelte:head>
   <title>Projects</title>
 </svelte:head>
 
-<Pie />
-<h1>{projects.length} Projects</h1>
+<Pie data={pieData} bind:selectedIndex={selectedYearIndex} />
+
+<input
+  type="search"
+  bind:value={query}
+  aria-label="Search projects"
+  placeholder="🔍 Search projects…"
+/>
+
+<h1>
+  {projectsToShow.length} Projects
+</h1>
 <div class="projects">
-  {#each projects as p}
+  {#each projectsToShow as p}
     <Project info={p} />
   {/each}
 </div>
